27a6f5a399fc746b335133ad2b1ba31acd1a84ff
HarshGundecha <harsh.gundecha@gmail.com>
2019-10-09 17:56:20 +0530

    [ADD] - springboot project

---
+++ src/main/java/org/dssec4/tweetitbackend/TweetitbackendApplication.java
@@ -0,0 +1,13 @@
+package org.dssec4.tweetitbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetitbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweetitbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/dssec4/tweetitbackend/TweetitbackendApplicationTests.java
@@ -0,0 +1,16 @@
+package org.dssec4.tweetitbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TweetitbackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
