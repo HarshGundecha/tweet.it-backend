4733ceb83f10ae95cb4876920f3c439f49e21d05
HarshGundecha <harsh.gundecha@gmail.com>
2019-11-13 00:41:38 +0530

    implement JpaRepository instead of CrudRepository

--- src/main/java/org/dssec4/tweetitbackend/repository/UserRepository.java
+++ src/main/java/org/dssec4/tweetitbackend/repository/UserRepository.java
@@ -1,12 +1,13 @@
 package org.dssec4.tweetitbackend.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import org.dssec4.tweetitbackend.entity.User;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 	
 	User findByUsername(String username);
 	
