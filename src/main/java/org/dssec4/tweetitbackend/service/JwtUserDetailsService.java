d1b614cbd4cd8df857d38a47ec1378aeebb0ca40
HarshGundecha <harsh.gundecha@gmail.com>
2019-11-13 00:43:21 +0530

    remove save user method

--- src/main/java/org/dssec4/tweetitbackend/service/JwtUserDetailsService.java
+++ src/main/java/org/dssec4/tweetitbackend/service/JwtUserDetailsService.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import org.dssec4.tweetitbackend.repository.UserRepository;
@@ -18,9 +17,6 @@ public class JwtUserDetailsService implements UserDetailsService {
 	@Autowired
 	private UserRepository userRepository;
 
-	@Autowired
-	private PasswordEncoder bcryptEncoder;
-
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		User user = userRepository.findByUsername(username);
@@ -29,12 +25,5 @@ public class JwtUserDetailsService implements UserDetailsService {
 		}
 		return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
 				new ArrayList<>());
-	}
-	
-	public User save(User user) {
-		User newUser = new User();
-		newUser.setUsername(user.getUsername());
-		newUser.setPassword(bcryptEncoder.encode(user.getPassword()));
-		return userRepository.save(newUser);
 	}
 }
