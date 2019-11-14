d7decdff9e302d999e2084df35e5a59a1c392347
HarshGundecha <harsh.gundecha@gmail.com>
2019-11-14 23:26:16 +0530

    add a single getComment

--- src/main/java/org/dssec4/tweetitbackend/service/CommentService.java
+++ src/main/java/org/dssec4/tweetitbackend/service/CommentService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CommentService {
@@ -19,4 +20,9 @@ public class CommentService {
     public List<Comment> getComments() {
         return commentRepository.findAll();
     }
+
+    public Optional<Comment> getComment(Long id) {
+        return commentRepository.findById(id);
+    }
+
 }
